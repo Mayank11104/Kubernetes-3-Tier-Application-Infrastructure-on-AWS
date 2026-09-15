248b74bcc72ebd25655813c5ecf867f12bbc1562
Mayank <mayuchaudhari2004@gmail.com>
2026-09-15 15:25:20 +0530

    feat(infra): add aws security group and networking rules

---
+++ terraform/security.tf
@@ -0,0 +1,42 @@
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "k8s_sg" {
+  name        = "k8s-sg"
+  description = "Security group for k8 allow ssh, http, https"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "Allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow HTTP"
+    to_port     = 80
+    from_port   = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow HTTPS"
+    to_port     = 443
+    from_port   = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
